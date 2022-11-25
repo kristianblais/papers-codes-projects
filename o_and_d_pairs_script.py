3b195fe2e512f51c42c039ea1d364c4a1504725c
Kristian Blais <kristian.blais@gmail.com>
2022-11-24 20:38:45 -0700

    Added comments and tested the functionality

--- o_and_d_pairs_script.py
+++ o_and_d_pairs_script.py
@@ -69,10 +69,43 @@ def distancematrix(origins, destinations, key, mode = "driving", as_matrix = Tru
 
     return matrix
 
+####################
+# Test Function
+####################
+
+# Load API key from text file
 with open('googlemaps_api_key.txt', 'r') as key_file:
     api_key = str(key_file.read().replace('\n', ''))
 
-origins = "University of British Columbia|Simon Fraser University|Capilano University"
-destinations = ["Universtiy of Victoria", "University of Vancouver Island", "Royal Roads University", "Quest University Squamish"]
+# test O&D pairs
+origins_dest = [
+    "University of Chicago Saieh Hall for Economics",
+    "University of Wisconsin Maidson Department of Economics",
+    "University of Michigan Ann Arbor Department of Economics",
+    "Northwestern Universtiy Department of Economics",
+    "University of Illinois Champaign Urbana Department of Economics",
+    "Noter Dayum University Department of Economics" # Bad input for test
+]
 
-test_matrix = distancematrix(origins, destinations, api_key)
+# Finding - Misspelt inputs will be autocorrected by Google (to a point)
+
+# test run of function 
+test_matrix = distancematrix(origins_dest, origins_dest, api_key)
+
+print(test_matrix)
+
+# Locations in Longitude/Latitude Format 
+o_and_d_lon_lat = [
+    "39.95331806714473, -75.19457011888244", # UPenn Department of Economics
+    "40.349063616582946, -74.65554434247127", # Princeton Department of Economics
+    "40.49975803330643, -74.44689473534827", # Rutgers Department of Economics
+    "40.80765159680687, -73.95879785887432", # Columbia Department of Economics
+    "41.31389963506103, -72.92395166846102" # Yale Department of Economics
+]
+
+long_lat_test_matrix = distancematrix(o_and_d_lon_lat, o_and_d_lon_lat, api_key)
+
+print(long_lat_test_matrix)
+
+# So even when locations are in Long/Lat format, it still returns the written address
+
